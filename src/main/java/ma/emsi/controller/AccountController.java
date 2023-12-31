a6c4a75ac1a98d2326adf6d07a02598eb60a2127
Elgbouri saad <71592569+elgbouriSaad@users.noreply.github.com>
2023-12-31 16:27:55 +0100

    add account controller

--- src/main/java/ma/emsi/controller/AccountController.java
+++ src/main/java/ma/emsi/controller/AccountController.java
@@ -1,9 +1,15 @@
 package ma.emsi.controller;
 
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import ma.emsi.model.Account;
+import ma.emsi.model.Objective;
 import ma.emsi.service.AccountService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/account")
@@ -12,8 +18,53 @@ public class AccountController {
 
     private final AccountService accountService;
 
-    @RequestMapping("/add")
-    public void addAccount() {
-    System.out.println("hello");
+    @PostMapping
+    public ResponseEntity<?> createAccount(@Valid @RequestBody Account account) {
+        try {
+            Account createdAccount = accountService.createAccount(account);
+            return new ResponseEntity<>(createdAccount, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @GetMapping("/{accountId}")
+    public ResponseEntity<?> getAccountById(@PathVariable int accountId) {
+        try {
+            Account account = accountService.getAccountById(accountId);
+            return ResponseEntity.ok(account);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @GetMapping("/{accountId}/objectives")
+    public ResponseEntity<?> getObjectivesForAccount(@PathVariable int accountId) {
+        try {
+            List<Objective> objectives = accountService.getObjectivesForAccount(accountId);
+            return ResponseEntity.ok(objectives);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PutMapping("/{accountId}")
+    public ResponseEntity<?> updateAccount(@PathVariable int accountId, @Valid @RequestBody Account updatedAccount) {
+        try {
+            Account account = accountService.updateAccount(accountId, updatedAccount);
+            return ResponseEntity.ok(account);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @DeleteMapping("/{accountId}")
+    public ResponseEntity<Void> deleteAccount(@PathVariable int accountId) {
+        try {
+            accountService.deleteAccount(accountId);
+            return ResponseEntity.noContent().build();
+        } catch (Exception e) {
+            return new ResponseEntity(e.getMessage(), HttpStatus.NOT_FOUND);
+        }
     }
 }
--- src/main/java/ma/emsi/service/AccountService.java
+++ src/main/java/ma/emsi/service/AccountService.java
@@ -1,9 +1,56 @@
 package ma.emsi.service;
 
-import lombok.AllArgsConstructor;
+import ma.emsi.model.Account;
+import ma.emsi.model.Objective;
+import ma.emsi.repository.AccountRepository;
+import ma.emsi.repository.ObjectiveRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
-@AllArgsConstructor
 public class AccountService {
+
+    private final AccountRepository accountRepository;
+    private final ObjectiveRepository objectiveRepository;
+
+    public AccountService(AccountRepository accountRepository, ObjectiveRepository objectiveRepository) {
+        this.accountRepository = accountRepository;
+        this.objectiveRepository = objectiveRepository;
+    }
+
+    @Transactional
+    public Account createAccount(Account account) {
+        // Perform any additional validation/business logic here before saving
+        return accountRepository.save(account);
+    }
+
+    public Account getAccountById(int accountId) {
+        return accountRepository.findById(accountId)
+                .orElseThrow(() -> new RuntimeException("Account not found with id: " + accountId));
+    }
+
+    public List<Objective> getObjectivesForAccount(int accountId) {
+        Account account = getAccountById(accountId);
+        return account.getObjectives();
+    }
+
+    @Transactional
+    public Account updateAccount(int accountId, Account updatedAccount) {
+        Account existingAccount = getAccountById(accountId);
+
+        // Perform any additional validation/business logic here before updating
+        existingAccount.setName(updatedAccount.getName());
+        existingAccount.setBalance(updatedAccount.getBalance());
+
+        return accountRepository.save(existingAccount);
+    }
+
+    @Transactional
+    public void deleteAccount(int accountId) {
+        Account account = getAccountById(accountId);
+        accountRepository.delete(account);
+    }
 }
